48573558334225bc71db32786bc6274bea89e7b0
Amit Rathore <amitrathore@gmail.com>
2009-08-17 14:33:05 -0700

    initial work around selenium helper

--- src/org/rathore/amit/utils/selenium.clj
+++ src/org/rathore/amit/utils/selenium.clj
@@ -1,24 +1,30 @@
 (ns org.rathore.amit.utils.selenium
-  (:import (com.thoughtworks.selenium DefaultSelenium)))
+  (:import (com.thoughtworks.selenium DefaultSelenium))
+  (:require (org.danlarkin [json :as json])))
 
-(def PAUSE-TIME-MILLIS 10000)
+(def PAUSE-TIME-MILLIS 20000)
 
-(defn sauce-labs-start-command []
-  (str "{\"username\": \"username-string\"," +
-                "\"access-key\": \"access-key-string\"," +
-                "\"os\": \"Linux\"," +
-                "\"browser\": \"firefox\"," +
-                "\"browser-version\": \"3.\"}",
-                "http://saucelabs.com/"))
+(defn sauce-labs-start-command [username-string access-key-string]
+  (json/encode-to-str
+   {"username" username-string
+    "access-key" access-key-string
+    "os" "Windows 2003"
+    "browser" "iexplore"
+    "browser-version" "7."}))
 
 (defn selenium-client [rc-server start-url]
   (DefaultSelenium. rc-server 4444 "*chrome" start-url))
 
+(defn saucy-client [start-url]
+  (DefaultSelenium. "saucelabs.com" 4444 (sauce-labs-start-command "amit" "c80c708c-5c7b-420c-b58c-032774a75d17") start-url))
+
 (defn pause-test []
   (Thread/sleep PAUSE-TIME-MILLIS))
 
-(defmacro with-selenium [[sel-name rc-server start-url] & body]
-  `(let [~sel-name (selenium-client ~rc-server ~start-url)]     
+(defmacro with-selenium [[sel-name local rc-server start-url] & body]
+  `(let [~sel-name (if ~local
+		     (selenium-client ~rc-server ~start-url)
+		     (saucy-client ~start-url))]
      (.start ~sel-name)
      ~@body
      (.stop ~sel-name)
