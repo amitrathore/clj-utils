e9fc595153286bd4ee7c0b402296d17b1ef2ca50
Amit Rathore <amitrathore@gmail.com>
2010-02-19 18:56:21 -0800

    initial work to reconnect to rabbitmq if exception thrown in the queue-seq

--- src/org/rathore/amit/utils/rabbitmq.clj
+++ src/org/rathore/amit/utils/rabbitmq.clj
@@ -1,7 +1,16 @@
 (ns org.rathore.amit.utils.rabbitmq
-  (:import (com.rabbitmq.client ConnectionParameters ConnectionFactory MessageProperties QueueingConsumer)))
-(use 'org.rathore.amit.utils.clojure)
-(use 'org.rathore.amit.utils.logger)
+  (:import (com.rabbitmq.client ConnectionParameters ConnectionFactory MessageProperties QueueingConsumer))
+  (:import (com.runa StableChannels))
+  (:use org.rathore.amit.utils.clojure org.rathore.amit.utils.logger)
+  (:use clojure.stacktrace))
+
+(defn new-multiplexer [q-host q-username q-password]
+  (let [stable-channels (StableChannels. q-host q-username q-password)]
+    (fn [accessor]
+      (cond 
+	(= accessor :new-channel) (.createChannel stable-channels)))))
+
+(def *rabbitmq-multiplexer*)
 
 (defn delivery-seq [ch q]
   (lazy-seq
@@ -9,13 +18,6 @@
           m (String. (.getBody d))]
       (.basicAck ch (.. d getEnvelope getDeliveryTag) false)
       (cons m (delivery-seq ch q)))))
-
-;(defn queue-seq [conn queue-name]
-;  (let [ch (.createChannel conn)]
-;    (.queueDeclare ch queue-name)
-;    (let [consumer (QueueingConsumer. ch)]
-;      (.basicConsume ch queue-name consumer)
-;      (delivery-seq ch consumer))))
 
 (defn queue-seq 
   ([conn queue-name]
@@ -25,10 +27,8 @@
        (.queueDeclare ch queue-name)
        (if (and exchange-name exchange-type)
          (do
-           (log-message "exchange declared with" exchange-name exchange-type)
            (.exchangeDeclare ch exchange-name exchange-type)
-           (.queueBind ch queue-name exchange-name ""))
-         (log-message "skipping exchanges!"))
+           (.queueBind ch queue-name exchange-name "")))
        (let [consumer (QueueingConsumer. ch)]
          (.basicConsume ch queue-name consumer)
          (delivery-seq ch consumer)))))
@@ -41,28 +41,33 @@
 	factory (ConnectionFactory. params)]
     (.newConnection factory q-host)))
 
+(defn do-with-connection [conn new-connection-thunk the-function])
+
+;(defmacro with-connection [connection q-host q-username q-password & exprs]
+;  `(with-open [~connection (new-connection-for ~q-host ~q-username ~q-password)]
+;     (do ~@exprs)))
+
 (defmacro with-connection [connection q-host q-username q-password & exprs]
-  `(with-open [~connection (new-connection-for ~q-host ~q-username ~q-password)]
-     (do ~@exprs)))
+  `(let [xx# (fn zz# [~connection]
+               (with-open [~connection ~connection]
+                 (try
+                  (do ~@exprs)
+                  (catch Exception e#
+                    (log-message "Catching...")
+                    (log-exception e#)
+                    (Thread/sleep 10000)
+                    (log-message "and recovering...")
+                    (zz# (new-connection-for ~q-host ~q-username ~q-password))))))]
+     (xx# (new-connection-for ~q-host ~q-username ~q-password))))
 
 (defn drop-on-channel 
   ([chan q-name q-message-string]
-     (drop-on-channel chan"" q-name q-message-string))
+     (drop-on-channel chan "" q-name q-message-string))
   ([chan exchange-name q-name q-message-string]
      (with-open [channel chan]
-       (log-message "drop-on-channel:" channel, "exchange-name:" exchange-name)
        ;q-declare args: queue-name, passive, durable, exclusive, autoDelete other-args-map                                  
        (.queueDeclare channel q-name) ; true false false auto-delete-queue (new java.util.HashMap))
-       (.basicPublish channel exchange-name q-name false true nil (.getBytes q-message-string))
-       (log-message "dropped-on-channel:" channel))))
-
-;(defn drop-on-new-channel [connection q-name q-message-string]
-;  (with-open [channel (.createChannel connection)]
-;    (drop-on-channel channel q-name q-message-string)))
-
-;(defn send-on-transport-amqp [q-host q-username q-password q-name q-message-string]
-;  (with-connection connection q-host q-username q-password
-;    (drop-on-new-channel connection q-name q-message-string)))
+       (.basicPublish channel exchange-name q-name false true nil (.getBytes q-message-string)))))
 
 (defn start-queue-message-handler-for-function-amqp 
   ([q-host q-username q-password q-name the-function]
@@ -71,3 +76,11 @@
      (with-connection connection q-host q-username q-password
        (doseq [message (queue-seq connection exchange-name exchange-type q-name)]
          (the-function message)))))
+
+(defn send-on-q 
+  ([q-name q-message-string]
+     (let [channel (*rabbitmq-multiplexer* :new-channel)]
+       (drop-on-channel channel q-name q-message-string)))
+  ([exchange-name q-name q-message-string]
+     (let [channel (*rabbitmq-multiplexer* :new-channel)]
+       (drop-on-channel channel exchange-name q-name q-message-string))))  
