760917eb9e22e04748b2492fe0abdf280c3ef68e
Amit Rathore <amitrathore@gmail.com>
2010-06-04 12:19:18 -0700

    made the project lein, and renamed the config var to *clj-utils-config*

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-utils "0.1.0"
+  :description "A bunch of common utilities for Clojure programs"
+  :url "http://github.com/amitrathore/clj-utils"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.clojars.sethtrain/postal "0.2.0"]
+                 [org.clojars.miau.biz/selenium-server-standalone "2.0a2"]
+                 [org.danlarkin/clojure-json "1.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/org/rathore/amit/utils/config.clj
+++ src/org/rathore/amit/utils/config.clj
@@ -1,50 +1,42 @@
 (ns org.rathore.amit.utils.config
   (:use org.rathore.amit.utils.process))
 
-(def *rathore-utils-config* nil)
+(def *clj-utils-config*)
 ;; config is a hash with the following keys
 ;; :log-to-console -> boolean, if true, logger will echo messages to the console
 ;; :logs-dir -> points to the directory where log files must be kept
 ;; :log-filename-prefix -> to prepend to the log filename, used as a visual identifier when there are lots of log files
 ;; :exception-notifier -> a hash containing :enabled (boolean), :from, :to, :subject-prefix
 
-(defmacro with-config [& exprs]
-  `(if (nil? *rathore-utils-config*)
-     (throw (Exception. "*rathore-utils-config* is not bound appropriately!"))
+(defmacro with-notifier-config [& exprs]
+  `(if (nil? (:exception-notifier *clj-utils-config*))
+     (throw (Exception. "exception-notifier not configured inside *clj-utils-config*"))
      (do ~@exprs)))
 
-(defmacro with-notifier-config [& exprs]
-  `(with-config
-    (if (nil? (:exception-notifier *rathore-utils-config*))
-      (throw (Exception. "exception-notifier not configured inside *rathore-utils-config*"))
-      (do ~@exprs))))
-
 (defn should-log-to-console? []
-  (with-config
-   (:log-to-console *rathore-utils-config*)))
+  (:log-to-console *clj-utils-config*))
 
 (defn notify-on-exception? []
   (with-notifier-config
-   (let [en-settings (:exception-notifier *rathore-utils-config*)]
+   (let [en-settings (:exception-notifier *clj-utils-config*)]
      (:enabled en-settings))))
 
 (defn error-notification-from []
   (with-notifier-config
-   (let [en-settings (:exception-notifier *rathore-utils-config*)]
+   (let [en-settings (:exception-notifier *clj-utils-config*)]
      (:from en-settings))))
 
 (defn error-notification-to []
   (with-notifier-config
-   (let [en-settings (:exception-notifier *rathore-utils-config*)]
+   (let [en-settings (:exception-notifier *clj-utils-config*)]
      (:to en-settings))))
 
 (defn error-notification-subject-prefix []
   (with-notifier-config
-   (let [en-settings (:exception-notifier *rathore-utils-config*)]
+   (let [en-settings (:exception-notifier *clj-utils-config*)]
      (:subject-prefix en-settings))))
 
 (defn log-file []
-  (with-config
-   (let [dirname (:logs-dir *rathore-utils-config*)
-	 prefix (:log-filename-prefix *rathore-utils-config*)]
-     (str  dirname "/"  prefix "_" (process-pid) ".log"))))
+  (let [dirname (:logs-dir *clj-utils-config*)
+        prefix (:log-filename-prefix *clj-utils-config*)]
+    (str  dirname "/"  prefix "_" (process-pid) ".log")))
