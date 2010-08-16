946aa1cf7c059ef3e18e972088fee178c2a11614
Amit Rathore <amitrathore@gmail.com>
2010-08-16 10:56:22 -0700

    added logging pool state

--- src/org/rathore/amit/utils/rabbitmq.clj
+++ src/org/rathore/amit/utils/rabbitmq.clj
@@ -19,6 +19,7 @@
   (Thread/sleep (* 1000 n)))
 
 (defn create-channel-guaranteed []
+  (log-message "[Rabbit: active idle max]" (pool-status))
   (let [c (get-connection-from-pool)] ;;is outside try, so rabbit-down-exception bubbles up
     (try 
      (let [ch (.createChannel c)]
